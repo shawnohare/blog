78138bfac225b6c431804744dc48b66e85d02291
Shawn O'Hare <ohare.shawn@gmail.com>
2016-04-25 00:05:23 -0700

    WIP untested circleci integration.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 readonly PUBLISH_REPO="https://github.com/shawnohare/shawnohare.github.io"
 readonly PUBLISH_DIR="shawnohare.github.io"
-readonly GENERATOR_REPO_NAME="shawnohare/blog"
+readonly GENERATOR_REPO_NAME="shawnohare.com"
 readonly HUGO_PUBLIC_DIR="public"
 readonly THEMES_DIR="themes"
 readonly THEME_REPO="https://github.com/shawnohare/hugo-morphism"
