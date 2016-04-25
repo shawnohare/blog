793a6851252e08fa027152fcdc8a645b01893544
Shawn O'Hare <ohare.shawn@gmail.com>
2016-04-25 08:08:07 -0700

    Fix overwritten deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,33 @@
-readonly PUBLISH_REPO="https://github.com/shawnohare/shawnohare.github.io"
-readonly PUBLISH_DIR="shawnohare.github.io"
-readonly GENERATOR_REPO_NAME="shawnohare.com"
-readonly HUGO_PUBLIC_DIR="public"
-readonly THEMES_DIR="themes"
-readonly THEME_REPO="https://github.com/shawnohare/hugo-morphism"
-readonly REV=$(git rev-parse HEAD)
+readonly publish_repo="https://github.com/shawnohare/shawnohare.github.io"
+readonly publish_dir="shawnohare.github.io"
+readonly generator="shawnohare.com"
+readonly public_dir="public"
+readonly themes_dir="themes"
+readonly themes_repo="https://github.com/shawnohare/hugo-morphism"
+readonly theme="hugo-morphism"
+readonly theme_path="${themes_dir}/${theme}"
+readonly rev=$(git rev-parse HEAD)
 
 
-if [[ -d "${PUBLISH_DIR}" ]]; then
-  git clone "${PUBLISH_REPO}" "${PUBLISH_DIR}"
+mkdir -p "${themes_dir}" 
+
+if [[ ! -d "${publish_dir}" ]]; then
+  git clone "${publish_repo}" "${publish_dir}"
 fi
 
-if [[ -d "${THEMES_DIR}/${THEME_REPO}" ]]; then
-  mkdir -p "${THEMES_DIR}"
-  git clone "${THEME_REPO}" "${THEMES_DIR}/${THEME_REPO}"
+if [[ ! -d "${theme_path}" ]]; then
+  git clone "${themes_repo}" "${theme_path}"
+else 
+  cd "${theme_path}"
+  git pull
+  cd ../..
 fi
 
-hugo -d "${HUGO_PUBLIC_DIR}"
-cp -r "${HUGO_PUBLIC_DIR}/*" "${PUBLISH_DIR}"
-cd "${PUBLISH_DIR}"
+hugo -d "${public_dir}"
+cp -r "${public_dir}"/* "${publish_dir}"
+cd "${publish_dir}"
 git add .
-git commit -m "Generated from ${GENERATOR_REPO_NAME} commit ${REV}"
+git commit -m "Generated from ${generator} commit ${rev}"
 git push -f
 cd ..
 
