ec8871589caa914fa011a5dfad94c09f610ebb82
Shawn O'Hare <ohare.shawn@gmail.com>
2016-04-26 10:53:23 -0700

    Add clean mode to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,21 @@
 readonly publish_repo="https://github.com/shawnohare/shawnohare.github.io"
-readonly publish_dir="shawnohare.github.io"
-readonly generator="shawnohare.com"
 readonly public_dir="public"
-readonly public_tmp="public_tmp"
 readonly themes_dir="themes"
 readonly themes_repo="https://github.com/shawnohare/hugo-morphism"
 readonly theme="hugo-morphism"
 readonly theme_path="${themes_dir}/${theme}"
+readonly generator="shawnohare.com"
 readonly rev=$(git rev-parse HEAD)
+clean="false"
 
 
 init() {
   echo "Initializing."
+  if [[ "${clean}" == "true" ]]; then
+    echo "Removing existing dirs."
+    rm -rf "${public_dir}"
+    rm -rf "${theme_path}"
+  fi
   return 0
 }
 
@@ -44,8 +48,8 @@ publish() {
     cleanup
     exit 1
   fi
+
   cd "${public_dir}"
-  # Init or re-init a git repo in the public dir hugo just created.
   git init
   git remote add origin "${publish_repo}" # this will fail
   git add .
@@ -66,6 +70,24 @@ main() {
   cleanup
   return 0
 }
+
+for i in "$@"; do
+  case $i in
+    -c|--clean)
+      echo "In clean mode.  Completely rebuilding site."
+      clean=true
+      shift
+      ;;
+    --default)
+      echo "Default mode."
+      shift
+      ;;
+    *)
+      echo "Unknown option. Exiting"
+      exit 1
+      ;;
+  esac
+done
 
 main
 
